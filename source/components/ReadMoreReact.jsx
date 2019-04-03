0b0bd9792ac98113bf3fc6ee36e5cbf3b80bf59c
Siddharth Gujrathi <sidh.gujrathi@gmail.com>
2019-04-03 15:19:37 +0530

    Fixed: next line adoptation while rendering text in span

--- source/components/ReadMoreReact.jsx
+++ source/components/ReadMoreReact.jsx
@@ -3,49 +3,49 @@ import PropTypes from 'prop-types';
 import trimText from '../utils/trimText'
 
 export default class ReadMoreReact extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 
-    let args = [
-      this.props.text,
-      this.props.min,
-      this.props.ideal,
-      this.props.max
-    ];
+		let args = [
+			this.props.text,
+			this.props.min,
+			this.props.ideal,
+			this.props.max
+		];
 
-    const [primaryText, secondaryText] = trimText(...args);
-		this.state = { displaySecondary: false, primaryText , secondaryText, readMoreText: this.props.readMoreText };
+		const [primaryText, secondaryText] = trimText(...args);
+		this.state = { displaySecondary: false, primaryText, secondaryText, readMoreText: this.props.readMoreText };
 	}
 
 	setStatus() {
 		let display = !this.state.displaySecondary;
-		this.setState({displaySecondary: display});
+		this.setState({ displaySecondary: display });
 	}
 
 	render() {
 		let displayText;
 		if (!this.state.secondaryText) {
 			displayText = (
-			<div className="display-text-group">
-				<span className="displayed-text">
-					{`${this.state.primaryText} ${this.state.secondaryText}`}
-				</span>
-			</div>);
+				<div className="display-text-group">
+					<span className="displayed-text" style={{ whiteSpace: 'pre-wrap' }}>
+						{`${this.state.primaryText} ${this.state.secondaryText}`}
+					</span>
+				</div>);
 		}
 		else if (this.state.displaySecondary) {
 			displayText = (
-			<div className="display-text-group">
-				<span className="displayed-text"
-					onClick={this.setStatus.bind(this)}>
-					{`${this.state.primaryText} ${this.state.secondaryText}`}
-				</span>
-			</div>);
+				<div className="display-text-group">
+					<span className="displayed-text" style={{ whiteSpace: 'pre-wrap' }}
+						onClick={this.setStatus.bind(this)}>
+						{`${this.state.primaryText} ${this.state.secondaryText}`}
+					</span>
+				</div>);
 		} else {
 			displayText = (<div className="display-text-group">
-				<span className="displayed-text">
-          {this.state.primaryText}<span style={{ display: "none"}}>{this.state.secondaryText}</span>
-				<div className="read-more-button"
-						 onClick={this.setStatus.bind(this)}>{this.state.readMoreText}</div>
+				<span className="displayed-text" style={{ whiteSpace: 'pre-wrap' }}>
+					{this.state.primaryText}<span style={{ display: "none" }}>{this.state.secondaryText}</span>
+					<div className="read-more-button"
+						onClick={this.setStatus.bind(this)}>{this.state.readMoreText}</div>
 				</span>
 			</div>);
 		}
@@ -55,13 +55,13 @@ export default class ReadMoreReact extends React.Component {
 }
 
 ReadMoreReact.propTypes = {
-  text: PropTypes.string.isRequired,
-  min: PropTypes.number,
-  ideal: PropTypes.number,
-  max: PropTypes.number,
+	text: PropTypes.string.isRequired,
+	min: PropTypes.number,
+	ideal: PropTypes.number,
+	max: PropTypes.number,
 	readMoreText: PropTypes.string,
 };
 
 ReadMoreReact.defaultProps = {
-  readMoreText: "read more",
+	readMoreText: "read more",
 };
